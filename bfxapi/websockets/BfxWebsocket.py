5554d114542486939647c78ba45b8a6317ae5958
Jacob Plaster <jacob.plaster@youview.com>
2018-11-20 10:33:43 +0000

    Default host to api.bitfinex.com

--- bfxapi/websockets/BfxWebsocket.py
+++ bfxapi/websockets/BfxWebsocket.py
@@ -116,7 +116,7 @@ class BfxWebsocket(GenericWebsocket):
     20061: 'Websocket server resync complete'
   }
 
-  def __init__(self, API_KEY=None, API_SECRET=None, host='wss://test.bitfinex.com/ws/2',
+  def __init__(self, API_KEY=None, API_SECRET=None, host='wss://api.bitfinex.com/ws/2',
       onSeedCandleHook=None, onSeedTradeHook=None, *args, **kwargs):
     self.channels = {}
     self.API_KEY=API_KEY
