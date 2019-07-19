ef3dfe1d408fdc1334f3a39e5c59c23cef8766f3
Jacob Plaster <jacob.plaster@youview.com>
2019-07-19 14:33:13 +0700

    examples/ws: add derivative status updates example

---
+++ bfxapi/examples/ws/subscribe_derivative_status.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.append('../')
+
+from bfxapi import Client
+
+bfx = Client(
+  logLevel='INFO'
+)
+
+@bfx.ws.on('error')
+def log_error(err):
+  print ("Error: {}".format(err))
+
+@bfx.ws.on('status_update')
+def log_msg(msg):
+  print (msg)
+
+async def start():
+  await bfx.ws.subscribe_derivative_status('tBTCF0:USTF0')
+
+bfx.ws.on('connected', start)
+bfx.ws.run()
