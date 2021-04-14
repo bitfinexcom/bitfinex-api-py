e1f52543723929c491ac49d1bb1fa3558bd4a6f0
Jon <jonathanhon@hotmail.com>
2021-04-14 11:53:05 +0100

    Adding balance_available to the wallet (#132)
    * Adding balance_available to the wallet
    * Refactoring bfx_rest.py/get_wallets to use the updated Wallet class, which has available_balance i it
    
    Co-authored-by: jon <fcmisc@gmail.com>

--- bfxapi/websockets/wallet_manager.py
+++ bfxapi/websockets/wallet_manager.py
@@ -17,13 +17,13 @@ class WalletManager:
         wData = raw_ws_data[2]
         self.wallets = {}
         for wallet in wData:
-            new_wallet = Wallet(wallet[0], wallet[1], wallet[2], wallet[3])
+            new_wallet = Wallet(wallet[0], wallet[1], wallet[2], wallet[3], wallet[4])
             self.wallets[new_wallet.key] = new_wallet
         return self.get_wallets()
 
     def _update_from_event(self, raw_ws_data):
         wallet = raw_ws_data[2]
-        new_wallet = Wallet(wallet[0], wallet[1], wallet[2], wallet[3])
+        new_wallet = Wallet(wallet[0], wallet[1], wallet[2], wallet[3], wallet[4])
         self.wallets[new_wallet.key] = new_wallet
         return new_wallet
 
