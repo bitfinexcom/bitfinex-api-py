ae234b60662615abe12ec61421c43020349ba767
itsdeka <dario.moceri@bitfinex.com>
2021-12-20 12:41:33 +0100

    async fix

--- bfxapi/utils/decorators.py
+++ bfxapi/utils/decorators.py
@@ -2,10 +2,10 @@ from ..utils.custom_logger import CustomLogger
 
 
 def handle_failure(func):
-    def inner_function(*args, **kwargs):
+    async def inner_function(*args, **kwargs):
         logger = CustomLogger('BfxWebsocket', logLevel="DEBUG")
         try:
-            func(*args, **kwargs)
+            await func(*args, **kwargs)
         except Exception as exception_message:
             logger.error(exception_message)
 
