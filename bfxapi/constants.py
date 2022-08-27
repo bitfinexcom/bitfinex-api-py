17646f9980239aa9e3857cb6cf8c825cc9aedb59
itsdeka <dario.moceri@bitfinex.com>
2022-08-27 17:09:00 +0200

    refactoring - moved hosts to constants.py

---
+++ bfxapi/constants.py
@@ -0,0 +1,4 @@
+REST_HOST = 'https://api.bitfinex.com/v2'
+WS_HOST = 'wss://api.bitfinex.com/ws/2'
+PUB_REST_HOST = 'https://api-pub.bitfinex.com/v2'
+PUB_WS_HOST = 'wss://api-pub.bitfinex.com/ws/2'
