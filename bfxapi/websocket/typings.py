54542ac23b3449cc4b3ee3016b920be8e8a1df21
Davide Casale <davi0kprogramsthings@gmail.com>
2022-11-10 12:24:50 +0100

    Add bfxapi/websocket/typing.py script and define some custom types.

---
+++ bfxapi/websocket/typings.py
@@ -0,0 +1,16 @@
+from typing import TypedDict, Optional
+
+class BalanceUpdateStream(TypedDict):
+    AUM: float
+    AUM_NET: float
+
+class WalletUpdateStream(TypedDict):
+    WALLET_TYPE: str
+    CURRENCY: str
+    BALANCE: float
+    UNSETTLED_INTEREST: float
+    BALANCE_AVAILABLE: Optional[float]
+    DESCRIPTION: str
+    META: dict
+
+WalletSnapshotStream = list[WalletUpdateStream]
