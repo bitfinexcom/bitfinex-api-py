d9ecbaa9f01910ee1719d26b6e670e27fb998ddc
Davide Casale <davidecasale2002@gmail.com>
2022-11-29 18:05:20 +0100

    Change exception formatting inside error .add_listener in BfxWebsocketClient.

--- bfxapi/websocket/BfxWebsocketClient.py
+++ bfxapi/websocket/BfxWebsocketClient.py
@@ -33,7 +33,9 @@ class BfxWebsocketClient(object):
     def __init__(self, host, buckets=5, log_level = "ERROR", API_KEY=None, API_SECRET=None, filter=None):
         self.host, self.websocket, self.event_emitter = host, None, AsyncIOEventEmitter()
 
-        self.event_emitter.add_listener("error", lambda message: self.logger.error(str(message) + "\n" + traceback.format_exc()))
+        self.event_emitter.add_listener("error", 
+            lambda exception: self.logger.error("\n" + str().join(traceback.format_exception(type(exception), exception, exception.__traceback__))[:-1])
+        )
 
         self.API_KEY, self.API_SECRET, self.filter, self.authentication = API_KEY, API_SECRET, filter, False
 
