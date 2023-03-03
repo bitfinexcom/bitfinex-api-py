dc17b8f54ae7b47871499fffe26bd20ca62c0925
Davide Casale <davi0kprogramsthings@gmail.com>
2023-03-03 20:14:51 +0100

    Remove STAGING_REST_HOST and STAGING_WSS_HOST urls from bfxapi/urls.py.

--- bfxapi/urls.py
+++ bfxapi/urls.py
@@ -1,7 +1,5 @@
 REST_HOST = "https://api.bitfinex.com/v2"
 PUB_REST_HOST = "https://api-pub.bitfinex.com/v2"
-STAGING_REST_HOST = "https://api.staging.bitfinex.com/v2"
 
 WSS_HOST = "wss://api.bitfinex.com/ws/2"
 PUB_WSS_HOST = "wss://api-pub.bitfinex.com/ws/2"
-STAGING_WSS_HOST = "wss://api.staging.bitfinex.com/ws/2"
