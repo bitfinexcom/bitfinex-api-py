6c99d3aacfd6b779201f774fb3ee8bcbe6a5df18
Davide Casale <davi0kprogramsthings@gmail.com>
2023-03-06 16:03:08 +0100

    Add dev-requirements.txt. Split requirements between requirements.txt and dev-requirements.txt. Change install_requires in setup.py.

--- setup.py
+++ setup.py
@@ -33,19 +33,9 @@ setup(
         "bfxapi.rest", "bfxapi.rest.endpoints", "bfxapi.rest.middleware",
     ],
     install_requires=[
-        "certifi~=2022.12.7",
-        "charset-normalizer~=2.1.1",
-        "idna~=3.4",
-        "mypy~=0.991",
-        "mypy-extensions~=0.4.3",
         "pyee~=9.0.4",
-        "requests~=2.28.1",
-        "tomli~=2.0.1",
-        "types-requests~=2.28.11.5",
-        "types-urllib3~=1.26.25.4",
-        "typing_extensions~=4.4.0",
-        "urllib3~=1.26.13",
         "websockets~=10.4",
+        "requests~=2.28.1"
     ],
     python_requires=">=3.8"
 )
