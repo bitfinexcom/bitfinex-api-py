76f6ac9d33a80598d7cabd51af3120a91d9747b1
Davide Casale <davi0kprogramsthings@gmail.com>
2024-06-26 21:34:55 +0200

    Bump __version__ in file bfxapi/_version.py to v3.0.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="bitfinex-api-py",
-    version="3.0.2",
+    version="3.0.3",
     description="Official Bitfinex Python API",
     long_description=(
         "A Python reference implementation of the Bitfinex API "
