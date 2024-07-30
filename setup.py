398d79ccaa6100e1f17453bac23a95730d5947ca
Davide Casale <davi0kprogramsthings@gmail.com>
2024-07-30 05:22:36 +0200

    Add support to python 3.12.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="bitfinex,api,trading",
     project_urls={
